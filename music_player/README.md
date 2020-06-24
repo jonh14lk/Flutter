848db2f57f2e3f04e194ab2e0aa77c4df2533a58
joaoayalla <joaoayalla19@gmail.com>
2020-06-24 16:12:53 -0300

    finished homepage

--- music_player/README.md
+++ music_player/README.md
@@ -4,4 +4,7 @@ A music player application made in flutter
 
 ## Getting Started
 
+![GitHub Logo](https://raw.githubusercontent.com/jonh14lk/Flutter/master/music_player/assets/print2.jpeg)
+
 ![GitHub Logo](https://raw.githubusercontent.com/jonh14lk/Flutter/master/music_player/assets/print.jpeg)
+
--- music_player/lib/pages/inicio.dart
+++ music_player/lib/pages/inicio.dart
@@ -1,27 +1,104 @@
 import 'package:flutter/material.dart';
 
-Widget espacamento(double) {
-  return SizedBox(
-    height: double,
-  );
-}
-
 class Tela1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(children: <Widget>[
-        espacamento(50.0),
-        Text(
-          'Início',
-          style: TextStyle(
-            color: Colors.amberAccent[200],
-            letterSpacing: 2.0,
-            fontSize: 28.0,
-            fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height - 56.0,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/dj.jpg'),
+                          fit: BoxFit.cover)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(50.0)),
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Text(
+                                'PLAYLIST',
+                                style: TextStyle(
+                                    color: Colors.white.withOpacity(0.6)),
+                              ),
+                              Text('Playlist 1',
+                                  style: TextStyle(color: Colors.white)),
+                            ],
+                          ),
+                          Icon(
+                            Icons.playlist_add,
+                            color: Colors.white,
+                          )
+                        ],
+                      ),
+                      Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Icon(
+                            Icons.fast_rewind,
+                            color: Colors.white54,
+                            size: 42.0,
+                          ),
+                          SizedBox(width: 32.0),
+                          Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.blueAccent,
+                                  borderRadius: BorderRadius.circular(50.0)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Icon(
+                                  Icons.play_arrow,
+                                  size: 58.0,
+                                  color: Colors.white,
+                                ),
+                              )),
+                          SizedBox(width: 32.0),
+                          Icon(
+                            Icons.fast_forward,
+                            color: Colors.white54,
+                            size: 42.0,
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 45.0),
+                      Text('Nome da música',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 32.0)),
+                      SizedBox(
+                        height: 6.0,
+                      ),
+                      Text(
+                        'Nome da banda',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.6),
+                            fontSize: 18.0),
+                      ),
+                      SizedBox(height: 16.0),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
